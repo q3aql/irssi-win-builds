d645a7d0eb6919fcd652c35fb2709e1944fb5c6a
q3aql <q3aql@users.noreply.github.com>
2021-04-02 14:05:47 +0200

    Default mintty configuration (Irssi.cmd)

--- Irssi.cmd
+++ Irssi.cmd
@@ -8,4 +8,5 @@ IF NOT EXIST home\%USERNAME%\Irssi mkdir home\%USERNAME%\Irssi
 set CONFIG_FILE=/home/%USERNAME%/Irssi
 
 set TERMINFO_DIRS=terminfo
+copy /Y prompt\.minttyrc home\%USERNAME%
 bin\mintty.exe -t Irssi -i icon/Irssi.ico -e bin\irssi.exe --home=%CONFIG_FILE%
